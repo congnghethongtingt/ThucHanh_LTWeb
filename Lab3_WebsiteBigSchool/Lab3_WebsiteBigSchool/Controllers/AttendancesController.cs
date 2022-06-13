6e016d3f78bb9af2766e2a7a78e7ffa262a60fe6
GT <luugiathanh25@gmail.com>
2022-06-13 15:04:57 +0700

    tr55

---
+++ Lab3_WebsiteBigSchool/Lab3_WebsiteBigSchool/Controllers/AttendancesController.cs
@@ -0,0 +1,38 @@
+﻿using Lab3_WebsiteBigSchool.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace Lab3_WebsiteBigSchool.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int courseId)
+        {
+            var attendance = new Attendance
+            { 
+                CourseId = courseId,
+                //AttendeeId = User.Identity.GetUserId()
+            };
+
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
--- Lab3_WebsiteBigSchool/Lab3_WebsiteBigSchool/Models/ApplicationDbContext.cs
+++ Lab3_WebsiteBigSchool/Lab3_WebsiteBigSchool/Models/ApplicationDbContext.cs
@@ -12,6 +12,8 @@ namespace Lab3_WebsiteBigSchool.Models
 
         public DbSet<Course> Courses { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -20,6 +22,15 @@ namespace Lab3_WebsiteBigSchool.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Course)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
