38b6b46639c4e0238cd12d05b63871691869df4f
GT <luugiathanh25@gmail.com>
2022-06-20 09:10:54 +0700

    Bosung tr78

--- Lab3_WebsiteBigSchool/Lab3_WebsiteBigSchool/App_Start/BundleConfig.cs
+++ Lab3_WebsiteBigSchool/Lab3_WebsiteBigSchool/App_Start/BundleConfig.cs
@@ -20,7 +20,10 @@ namespace Lab3_WebsiteBigSchool
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.min.js"
+                      ));
+
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
